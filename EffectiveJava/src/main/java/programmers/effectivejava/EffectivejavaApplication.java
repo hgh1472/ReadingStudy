720631d22e0dec7f655526a5bd6cc35e569560b3
hgh1472 <hgh1472@naver.com>
2024-08-20 14:40:05 +0900

    init : 예시 코드를 위한 프로젝트 생성

---
+++ EffectiveJava/src/main/java/programmers/effectivejava/EffectivejavaApplication.java
@@ -0,0 +1,13 @@
+package programmers.effectivejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffectivejavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EffectivejavaApplication.class, args);
+	}
+
+}
---
+++ EffectiveJava/src/test/java/programmers/effectivejava/EffectivejavaApplicationTests.java
@@ -0,0 +1,13 @@
+package programmers.effectivejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffectivejavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
